15e42e92f7d1bd1eb9af2e022f61c5aafd8bafc7
WIlliam <136656642+ImNotTall@users.noreply.github.com>
2026-02-24 09:35:02 +1300

    Start GUI, starts rounds

---
+++ 01_Start_GUI_v1.py
@@ -0,0 +1,74 @@
+from tkinter import *
+
+class StartGame:
+
+    def __init__(self):
+
+        self.start_frame = Frame(padx=10, pady=10)
+        self.start_frame.grid()
+
+        intro_string = ("In each round you will be invited to choose a colour. your goal is"
+                        "to beat the target score and win the round (and keep your points).")
+
+        choose_string = "How many rounds do you want to play?"
+
+        start_labels_list = [
+            ["Colour Quest", ("Arial", 16, "bold"), None],
+            [intro_string, ("Arial", 12), None],
+            [choose_string, ("Arial", 12, "bold"), "#009900"]
+        ]
+
+        start_label_ref = []
+        for count, item in enumerate(start_labels_list):
+            make_label = Label(self.start_frame, text=item[0], font=item[1],
+                               fg=item[2],
+                               wraplength=350, justify="left", pady=10, padx=20)
+            make_label.grid(row=count)
+
+            start_label_ref.append(make_label)
+
+        self.choose_label = start_label_ref[2]
+
+        self.entry_area_frame = Frame(self.start_frame)
+        self.entry_area_frame.grid(row=3)
+
+        self.num_rounds_entry = Entry(self.entry_area_frame, font=("Arial", 20, "bold"),
+                                      width=10)
+        self.num_rounds_entry.grid(row=0, column=0, padx=10, pady=10)
+
+        self.play_button = Button(self.entry_area_frame, font=("Arial", 16, "bold"),
+                                  fg="#FFFFFF", bg="#0057D8", text="Play", width=10,
+                                  command=self.check_rounds)
+        self.play_button.grid(row=0, column=1)
+
+    def check_rounds(self):
+
+        rounds_wanted = self.num_rounds_entry.get()
+
+        self.choose_label.config(fg="#009900", font=("Arial", 12, "bold"))
+        self.num_rounds_entry.config(bg="#FFFFFF")
+
+        error = "Oops - Please choose a whole number more than zero."
+        has_errors = "no"
+
+        try:
+            rounds_wanted = int(rounds_wanted)
+            if rounds_wanted > 0:
+                self.choose_label.config(text=f"You have chosen to play {rounds_wanted} rounds")
+            else:
+                has_errors = "yes"
+
+        except ValueError:
+            has_errors = "yes"
+
+        if has_errors == "yes":
+            self.choose_label.config(text=error, fg="990000",
+                                     font=("Arial", 10, "bold"))
+            self.num_rounds_entry.config(bg="#F4CCCC")
+            self.num_rounds_entry.delete(0, END)
+
+if __name__ == "__main__":
+    root = Tk()
+    root.title("Colour Quest")
+    StartGame()
+    root.mainloop()
